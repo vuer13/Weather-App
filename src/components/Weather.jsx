5fca24303d02b657250626e1cbfa36a2ef1e7bb1
Ryan Vu <ryanvu1131@gmail.com>
2024-12-24 21:37:28 -0800

    Added more components

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -18,6 +18,22 @@ const Weather = () => {
       <img src={clearIcon} alt = "" className = 'weather-icon'/>
       <p className = 'temperature'>16°C</p>
       <p className= 'location'>London</p>
+      <div className='weather-data'>
+        <div className = 'col'>
+            <img src={humidityIcon} alt = ""/>
+            <div>
+                <p>91%</p>
+                <span>Humidity</span>
+            </div>
+        </div>
+        <div className = 'col'>
+            <img src={windIcon} alt = ""/>
+            <div>
+                <p>3.6 km/h</p>
+                <span>Wind Speed</span>
+            </div>
+        </div>
+      </div>
     </div>
   );
 };
